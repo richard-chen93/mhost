38a560627019daaeeea4e0197bbeb2764e430c83
Richard chen <maple34@126.com>
2020-08-21 14:16:52 +0800

    second commit

--- mhosts/urls.py
+++ mhosts/urls.py
@@ -7,7 +7,9 @@ from . import views
 urlpatterns = [
     # Home page.
     url(r'^$', views.index, name='index'),
-        # Show all topics.
+        # Show all host_types.
     url(r'^host_types/$', views.host_types, name='host_types'),
     
+    # Detail page for a single host_type.
+    url(r'^host_types/(?P<host_type_id>\d+)/$', views.host_type, name='host_type'),
 ]
